7e95b52b965f79d1883977c02561dc9fa407f7ff
SirJosh3917 <joshuaneedham@hotmail.com>
2018-06-17 12:29:34 -0400

    make way for revamp

--- StringDB/Inoperable/IInoperable.cs
+++
@@ -1,4 +0,0 @@
-﻿namespace StringDB.Inoperable {
-	/// <summary>If something inherits this, there's a very good chance it'll throw exceptions anytime you attempt to do anything at all.</summary>
-	public interface IInoperable { }
-}
