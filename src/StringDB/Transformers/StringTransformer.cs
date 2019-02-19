03c0e2e3cc29cf8525e142ebf169c60aa2ec68f1
SirJosh3917 <joshuaneedham@hotmail.com>
2019-02-18 23:28:00 -0500

    make string transformer

---
+++ src/StringDB/Transformers/StringTransformer.cs
@@ -0,0 +1,11 @@
+﻿using System.Text;
+
+namespace StringDB.Transformers
+{
+	public class StringTransformer : ITransformer<byte[], string>
+	{
+		public string Transform(byte[] pre) => Encoding.UTF8.GetString(pre);
+
+		public byte[] Transform(string post) => Encoding.UTF8.GetBytes(post);
+	}
+}
