279d4c7c815bf1754931e19d05334c89e9dce763
SirJosh3917 <joshuaneedham@hotmail.com>
2019-02-19 16:43:42 -0500

    add reverse transformer tests

---
+++ src/StringDB.Tests/ReverseTransformerTests.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace StringDB.Tests
+{
+	public class ReverseTransformerTests
+	{
+		public class A
+		{
+			public int Value { get; set; }
+		}
+
+		public class B
+		{
+			public int OtherValue { get; set; }
+		}
+
+		public class SimpleTransformer : ITransformer<A, B>
+		{
+			public B Transform(A pre) => new B { OtherValue = pre.Value };
+			public A Transform(B post) => new A { Value = post.OtherValue };
+		}
+
+		[Fact]
+		public void Reverses()
+		{
+			var reverse = new ReverseTransformer<B, A>(new SimpleTransformer());
+
+			reverse.Transform(new B { OtherValue = 3 })
+				.Value
+				.Should()
+				.Be(3);
+
+			reverse.Transform(new A { Value = 4 })
+				.OtherValue
+				.Should()
+				.Be(4);
+		}
+	}
+}
