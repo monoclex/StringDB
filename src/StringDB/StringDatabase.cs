0d02ef1fc553a80984b8979c5eb5f85f43507a44
SirJosh3917 <joshuaneedham@hotmail.com>
2019-02-22 23:11:17 -0500

    make "StringDatabase" :^)

---
+++ src/StringDB/StringDatabase.cs
@@ -0,0 +1,42 @@
+﻿using StringDB.Fluency;
+using StringDB.IO;
+using StringDB.Transformers;
+
+using System.IO;
+
+namespace StringDB
+{
+	public static class StringDatabase
+	{
+		public static IDatabase<string, string> Create()
+			=> new DatabaseBuilder()
+				.UseMemoryDatabase<string, string>();
+
+		public static IDatabase<string, string> Create
+		(
+			Stream stream,
+			bool leaveStreamOpen = false
+		)
+			=> Create(stream, StringDBVersions.Latest, leaveStreamOpen);
+
+		public static IDatabase<string, string> Create
+		(
+			Stream stream,
+			StringDBVersions version,
+			bool leaveStreamOpen
+		)
+			=> new DatabaseBuilder()
+				.UseIODatabase((builder) => builder.UseStringDB(version, stream, leaveStreamOpen))
+				.ApplyStringTransformation();
+
+		private static IDatabase<string, string> ApplyStringTransformation
+		(
+			this IDatabase<byte[], byte[]> database
+		)
+		{
+			var transformer = new StringTransformer();
+
+			return database.WithTransform(transformer, transformer);
+		}
+	}
+}
