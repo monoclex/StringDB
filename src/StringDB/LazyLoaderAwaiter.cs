bd05f693b4258f3d04b60ed1b435f58ed708817c
SirJosh3917 <joshuaneedham@hotmail.com>
2019-05-04 14:26:49 -0400

    add extension methods for lazy loaders

---
+++ src/StringDB/LazyLoaderAwaiter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace StringDB
+{
+	// note: no [PublicAPI]
+	/// <summary>
+	/// Used to allow an <see cref="ILazyLoader{T}"/> to be awaited.
+	/// </summary>
+	/// <typeparam name="T">The type of the value.</typeparam>
+	public struct LazyLoaderAwaiter<T> : INotifyCompletion
+	{
+		public ILazyLoader<T> LazyLoader;
+		private T _result;
+		public bool IsCompleted { get; private set; }
+
+		public T GetResult()
+		{
+			if (!IsCompleted)
+			{
+				_result = LazyLoader.Load();
+				IsCompleted = true;
+			}
+
+			return _result;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public void OnCompleted(Action continuation)
+			=> new Task(continuation).Start();
+	}
+}
---
+++ src/StringDB/LazyLoaderExtensions.cs
@@ -0,0 +1,41 @@
+﻿using JetBrains.Annotations;
+
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace StringDB
+{
+	/// <summary>
+	/// Extension methods on an <see cref="ILazyLoader{T}"/>.
+	/// </summary>
+	[PublicAPI]
+	public static class LazyLoaderExtensions
+	{
+		/// <summary>
+		/// Allows the usage of the await keyword for the <paramref name="lazyLoader"/>.
+		/// </summary>
+		/// <typeparam name="T">The type of the value.</typeparam>
+		/// <param name="lazyLoader">The lazy loader to get an awaiter for.</param>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static LazyLoaderAwaiter<T> GetAwaiter<T>(this ILazyLoader<T> lazyLoader)
+			=> new LazyLoaderAwaiter<T>
+			{
+				LazyLoader = lazyLoader
+			};
+
+		/// <summary>
+		/// Turns any ILazyLoader into a KeyValuePair with the key
+		/// </summary>
+		/// <typeparam name="TKey">The type of the key.</typeparam>
+		/// <typeparam name="TValue">The type of the value.</typeparam>
+		/// <param name="lazyLoader">The lazy loader.</param>
+		/// <param name="key">The key.</param>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static KeyValuePair<TKey, ILazyLoader<TValue>> ToKeyValuePair<TKey, TValue>
+		(
+			this ILazyLoader<TValue> lazyLoader,
+			TKey key
+		)
+			=> new KeyValuePair<TKey, ILazyLoader<TValue>>(key, lazyLoader);
+	}
+}
