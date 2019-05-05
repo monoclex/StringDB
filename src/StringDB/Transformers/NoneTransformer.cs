8824656f1494d7a3e6e002ee0c04a9885c021c52
SirJosh3917 <joshuaneedham@hotmail.com>
2019-05-04 22:15:35 -0400

    NoneTransformer that does nothing

---
+++ src/StringDB/Transformers/NoneTransformer.cs
@@ -0,0 +1,19 @@
+﻿using JetBrains.Annotations;
+
+namespace StringDB.Transformers
+{
+	/// <inheritdoc/>
+	/// <summary>
+	/// Applies absolutely no transformation to the objects given to it.
+	/// </summary>
+	/// <typeparam name="T">The type of object to do nothing to.</typeparam>
+	[PublicAPI]
+	public sealed class NoneTransformer<T> : ITransformer<T, T>
+	{
+		/// <inheritdoc/>
+		public T TransformPost(T post) => post;
+
+		/// <inheritdoc/>
+		public T TransformPre(T pre) => pre;
+	}
+}
---
+++ tests/StringDB.Tests/NoneTransformerTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+
+using StringDB.Transformers;
+
+using Xunit;
+
+namespace StringDB.Tests
+{
+	/// <summary>
+	/// Tests for a <see cref="NoneTransformer{T}"/>
+	/// </summary>
+	public class NoneTransformerTests
+	{
+		[Fact]
+		public void DoesLiterallyNothing()
+		{
+			var a = new NoneTransformer<int>();
+
+			a.TransformPre(7)
+				.Should().Be(7);
+
+			a.TransformPost(7)
+				.Should().Be(7);
+		}
+	}
+}
