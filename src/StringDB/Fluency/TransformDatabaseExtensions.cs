e706d5c45a526a644831fd3f3dd896660bf29a0b
SirJosh3917 <joshuaneedham@hotmail.com>
2019-05-05 22:44:57 -0400

    add key/value only transformations & documentation fixes

--- src/StringDB/Fluency/TransformDatabaseExtensions.cs
+++ src/StringDB/Fluency/TransformDatabaseExtensions.cs
@@ -1,19 +1,20 @@
 ﻿using JetBrains.Annotations;
 
 using StringDB.Databases;
+using StringDB.Transformers;
 
 using System.Runtime.CompilerServices;
 
 namespace StringDB.Fluency
 {
 	/// <summary>
-	/// Fluent extensions for a <see cref="TransformDatabase{TPreTransformKey,TPreTransformValue,TPostTransformKey,TPostTransformValue}"/>.
+	/// Fluent extensions for a <see cref="TransformDatabase{TPreKey,TPreValue,TPostKey,TPostValue}"/>.
 	/// </summary>
 	[PublicAPI]
 	public static class TransformDatabaseExtensions
 	{
 		/// <summary>
-		/// Apply a transformation to a database.
+		/// Applies a transformation to a database.
 		/// </summary>
 		/// <typeparam name="TPreKey">The database's key type.</typeparam>
 		/// <typeparam name="TPreValue">The database's value type.</typeparam>
@@ -23,7 +24,7 @@ namespace StringDB.Fluency
 		/// <param name="keyTransformer">A transformer for the keys.</param>
 		/// <param name="valueTransformer">A transformer for the values.</param>
 		/// <param name="disposeDatabase">If the underlying database should be disposed on dispose.</param>
-		/// <returns>A <see cref="TransformDatabase{TPreTransformKey,TPreTransformValue,TPostTransformKey,TPostTransformValue}"/>.</returns>
+		/// <returns>A <see cref="TransformDatabase{TPreKey,TPreValue,TPostKey,TPostValue}"/>.</returns>
 		[NotNull]
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static IDatabase<TPostKey, TPostValue> WithTransform<TPreKey, TPreValue, TPostKey, TPostValue>
@@ -37,6 +38,56 @@ namespace StringDB.Fluency
 			(
 				database,
 				keyTransformer,
+				valueTransformer,
+				disposeDatabase
+			);
+
+		/// <summary>
+		/// Apply a key-only transformation to a database.
+		/// </summary>
+		/// <typeparam name="TPreKey">The database's key type.</typeparam>
+		/// <typeparam name="TPostKey">The new type of key for the database.</typeparam>
+		/// <typeparam name="TValue">The database's value type.</typeparam>
+		/// <param name="database">The database to transform.</param>
+		/// <param name="keyTransformer">A transformer for the keys.</param>
+		/// <param name="disposeDatabase">If the underlying database should be disposed on dispose.</param>
+		/// <returns>A <see cref="TransformDatabase{TPreKey,TPreValue,TPostKey,TPostValue}"/>.</returns>
+		[NotNull]
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static IDatabase<TPostKey, TValue> WithKeyTransform<TPreKey, TPostKey, TValue>
+		(
+			[NotNull] this IDatabase<TPreKey, TValue> database,
+			[NotNull] ITransformer<TPreKey, TPostKey> keyTransformer,
+			bool disposeDatabase = true
+		)
+			=> database.WithTransform
+			(
+				keyTransformer,
+				NoneTransformer<TValue>.Default,
+				disposeDatabase
+			);
+
+		/// <summary>
+		/// Applies a value-only transformation to a database.
+		/// </summary>
+		/// <typeparam name="TKey">The database's key type.</typeparam>
+		/// <typeparam name="TPreValue">The database's value type.</typeparam>
+		/// <typeparam name="TPostValue">The new type of value for the database.</typeparam>
+		/// <param name="database">The database to transform.</param>
+		/// <param name="valueTransformer">A transformer for the values.</param>
+		/// <param name="disposeDatabase">If the underlying database should be disposed on dispose.</param>
+		/// <returns>A <see cref="TransformDatabase{TPreKey,TPreValue,TPostKey,TPostValue}"/>.</returns>
+		[NotNull]
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static IDatabase<TKey, TPostValue> WithValueTransform<TKey, TPreValue, TPostValue>
+		(
+			[NotNull] this IDatabase<TKey, TPreValue> database,
+			[NotNull] ITransformer<TPreValue, TPostValue> valueTransformer,
+			bool disposeDatabase = true
+		)
+			=> database.WithTransform
+			(
+				NoneTransformer<TKey>.Default,
 				valueTransformer,
 				disposeDatabase
 			);
