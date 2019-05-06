323bac43efb3b729fc8627f5da780b23a371e6a8
SirJosh3917 <joshuaneedham@hotmail.com>
2019-05-06 17:49:15 -0400

    add optimal token

---
+++ src/StringDB/IO/OptimalToken.cs
@@ -0,0 +1,41 @@
+﻿using JetBrains.Annotations;
+
+namespace StringDB.IO
+{
+	/// <summary>
+	/// Controls the value for an <see cref="IOptimalToken"/>
+	/// </summary>
+	[PublicAPI]
+	public interface IOptimalTokenSource : IOptimalToken
+	{
+		/// <summary>
+		/// Sets the value for the optimal reading time.
+		/// </summary>
+		void SetOptimalReadingTime(bool value);
+	}
+
+	/// <summary>
+	/// Represents the optimal time to read values from a file.
+	/// </summary>
+	[PublicAPI]
+	public interface IOptimalToken
+	{
+		/// <summary>
+		/// If it is the optimal reading time.
+		/// </summary>
+		bool OptimalReadingTime { get; }
+	}
+
+	/// <summary>
+	/// The default optimal reading token implementation.
+	/// </summary>
+	[PublicAPI]
+	public sealed class OptimalToken : IOptimalTokenSource
+	{
+		/// <inheritdoc/>
+		public bool OptimalReadingTime { get; set; }
+
+		/// <inheritdoc/>
+		public void SetOptimalReadingTime(bool value) => OptimalReadingTime = value;
+	}
+}
---
+++ tests/StringDB.Tests/OptimalTokenTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+
+using StringDB.IO;
+
+using Xunit;
+
+namespace StringDB.Tests
+{
+	/// <summary>
+	/// Tests for an <see cref="OptimalToken"/>
+	/// </summary>
+	public class OptimalTokenTests
+	{
+		/// <summary>
+		/// Test that we can set the token to an on and off state.
+		/// </summary>
+		[Fact]
+		public void SettingTokenStateWorks()
+		{
+			IOptimalTokenSource optimalToken = new OptimalToken();
+
+			optimalToken.OptimalReadingTime.Should().Be(false);
+
+			SetRead(true);
+			SetRead(false);
+
+			void SetRead(bool value)
+			{
+				optimalToken.SetOptimalReadingTime(value);
+				optimalToken.OptimalReadingTime.Should().Be(value);
+			}
+		}
+	}
+}
