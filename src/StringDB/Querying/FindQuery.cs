e0d8f0ffbd6854b8fb4812ba99a580287872124e
SirJosh3917 <joshuaneedham@hotmail.com>
2019-06-07 21:08:31 -0400

    find query creation

---
+++ src/StringDB/Querying/FindQuery.cs
@@ -0,0 +1,63 @@
+﻿using JetBrains.Annotations;
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StringDB.Querying
+{
+	/// <summary>
+	/// Represents a query used for finding a value.
+	/// </summary>
+	/// <typeparam name="TKey">The type of key.</typeparam>
+	/// <typeparam name="TValue">The type of value.</typeparam>
+	public class FindQuery<TKey, TValue> : IQuery<TKey, TValue>
+	{
+		private readonly Func<TKey, bool> _isItem;
+		private readonly CancellationToken _cancellationToken;
+
+		public FindQuery
+		(
+			Func<TKey, bool> isItem,
+			CancellationToken cancellationToken = default
+		)
+		{
+			_isItem = isItem;
+			_cancellationToken = cancellationToken;
+		}
+
+		/// <summary>
+		/// The key as a result of processing.
+		/// </summary>
+		public TKey Key { get; private set; }
+
+		/// <summary>
+		/// The value as a result of processing.
+		/// </summary>
+		public TValue Value { get; private set; }
+
+		public bool IsCancellationRequested => _cancellationToken.IsCancellationRequested;
+
+		public async Task<QueryAcceptance> Accept([NotNull] TKey key, [NotNull] IRequest<TValue> value)
+		{
+			await Task.Yield();
+
+			if (_isItem(key))
+			{
+				return QueryAcceptance.Completed;
+			}
+
+			return QueryAcceptance.NotAccepted;
+		}
+
+		public async Task Process([NotNull] TKey key, [NotNull] IRequest<TValue> value)
+		{
+			Key = key;
+			Value = await value.Request().ConfigureAwait(false);
+		}
+
+		public void Dispose()
+		{
+		}
+	}
+}
---
+++ src/StringDB/Querying/QueryManagerExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StringDB.Querying
+{
+	/// <summary>
+	/// Extension methods that can be applied to an <see cref="IQueryManager{TKey, TValue}"/>.
+	/// </summary>
+	public static class QueryManagerExtensions
+	{
+		/// <summary>
+		/// Finds the key value pair for a given key in a query manager.
+		/// </summary>
+		/// <typeparam name="TKey">The type of key.</typeparam>
+		/// <typeparam name="TValue">The type of value.</typeparam>
+		/// <param name="queryManager">The query manager to use.</param>
+		/// <param name="isKey">The delegate that determines if the item
+		/// is the one you are looking for.</param>
+		/// <param name="cancellationToken">A cancellation token to request
+		/// the cancellation of the query.</param>
+		/// <returns>An awaitable task that will return your key
+		/// and value.</returns>
+		public static async Task<KeyValuePair<TKey, TValue>> Find<TKey, TValue>
+		(
+			this IQueryManager<TKey, TValue> queryManager,
+			Func<TKey, bool> isKey,
+			CancellationToken cancellationToken = default
+		)
+		{
+			var query = new FindQuery<TKey, TValue>(isKey, cancellationToken);
+
+			await queryManager.ExecuteQuery(query).ConfigureAwait(false);
+
+			return new KeyValuePair<TKey, TValue>(query.Key, query.Value);
+		}
+	}
+}
