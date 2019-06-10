02fd6244856f20d804df2eccf5d2d54abef67e25
SirJosh3917 <joshuaneedham@hotmail.com>
2019-06-10 00:02:41 -0400

    interfaces for query pool and item

---
+++ src/StringDB/Querying/QueryItem.cs
@@ -0,0 +1,33 @@
+﻿using StringDB.Querying.Queries;
+
+using System.Threading.Tasks;
+
+namespace StringDB.Querying
+{
+	/// <summary>
+	/// Used by an <see cref="IQueryPool{TKey, TValue}"/>
+	/// to manage the query.
+	/// </summary>
+	/// <typeparam name="TKey">The type of key.</typeparam>
+	/// <typeparam name="TValue">The type of value.</typeparam>
+	public struct QueryItem<TKey, TValue>
+	{
+		/// <summary>
+		/// The completion source for the query. Setting this
+		/// should directly affect the completion state of the
+		/// query, from <see cref="IQueryManager{TKey, TValue}.ExecuteQuery(IQuery{TKey, TValue})"/>.
+		/// </summary>
+		public TaskCompletionSource<bool> CompletionSource;
+
+		/// <summary>
+		/// The query this query item focuses on.
+		/// </summary>
+		public IQuery<TKey, TValue> Query;
+
+		/// <summary>
+		/// The index this query item was appended to
+		/// during a database search.
+		/// </summary>
+		public int Index;
+	}
+}
