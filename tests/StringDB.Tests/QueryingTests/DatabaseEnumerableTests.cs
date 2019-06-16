dd1535319c73c33dca4f75779e86d1cba244070c
SirJosh3917 <joshuaneedham@hotmail.com>
2019-06-16 19:56:11 -0400

    add SimpleDatabaseValueRequest tests & work on DB enumerable tests

---
+++ tests/StringDB.Tests/QueryingTests/DatabaseEnumerableTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using StringDB.Querying;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace StringDB.Tests.QueryingTests
+{
+	public class DatabaseEnumerableTests
+	{
+		[Fact]
+		public void ModifyValue()
+		{
+			var current = 0;
+			foreach(var i in DatabaseEnumerable.ModifyValue(new KeyValuePair<int, int>[]
+			{
+				KeyValuePair.Create(0, 0),
+				KeyValuePair.Create(1, 1),
+				KeyValuePair.Create(2, 2),
+				KeyValuePair.Create(3, 3),
+				KeyValuePair.Create(4, 4),
+			}, @in => @in * 5))
+			{
+				i.Key.Should().Be(current);
+				i.Value.Should().Be(current * 5);
+
+				current++;
+			}
+		}
+	}
+}
--- tests/StringDB.Tests/QueryingTests/SimpleDatabaseValueRequestTests.cs
+++ tests/StringDB.Tests/QueryingTests/SimpleDatabaseValueRequestTests.cs
@@ -3,7 +3,7 @@
 using Moq;
 
 using StringDB.Querying;
-
+using System;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -14,24 +14,34 @@ namespace StringDB.Tests.QueryingTests.cs
 {
 	public class SimpleDatabaseValueRequestTests
 	{
+		private readonly Mock<ILazyLoader<int>> _loader;
+		private readonly SemaphoreSlim _lock;
+		private readonly SimpleDatabaseValueRequest<int> _sdvr;
+
+		public SimpleDatabaseValueRequestTests()
+		{
+			_loader = new Mock<ILazyLoader<int>>();
+			_loader.Setup(x => x.Load()).Returns(7);
+
+			_lock = new SemaphoreSlim(1);
+
+			_sdvr = new SimpleDatabaseValueRequest<int>
+			(
+				_loader.Object,
+				_lock
+			);
+		}
+
 		[Fact]
 		public async Task OnlyOne_CallToLazyLoader_IsMade_WhenMutlipleThreads_RequestData()
 		{
-			var lazyLoader = new Mock<ILazyLoader<int>>();
-			lazyLoader.Setup(x => x.Load()).Returns(7);
-
 			var mres = new ManualResetEventSlim(false);
-			var sdvr = new SimpleDatabaseValueRequest<int>
-			(
-				lazyLoader.Object,
-				new SemaphoreSlim(1)
-			);
 
 			var tasks = Enumerable.Repeat(Task.Run(async () =>
 			{
 				mres.Wait();
 
-				var result = await sdvr.Request()
+				var result = await _sdvr.Request()
 					.ConfigureAwait(false);
 
 				result.Should().Be(7);
@@ -42,7 +52,26 @@ namespace StringDB.Tests.QueryingTests.cs
 
 			await Task.WhenAll(tasks).ConfigureAwait(false);
 
-			lazyLoader.Verify(x => x.Load(), Times.Once());
+			_loader.Verify(x => x.Load(), Times.Once());
+		}
+
+		[Fact]
+		public async Task RepeatCalls_GetCached()
+		{
+			var result = await _sdvr.Request().ConfigureAwait(false);
+			result.Should().Be(7);
+
+			result = await _sdvr.Request().ConfigureAwait(false);
+			result.Should().Be(7);
+
+			_loader.Verify(x => x.Load(), Times.Once());
+		}
+
+		// lol
+		[Fact]
+		public void Disposing_DoesntFail()
+		{
+			_sdvr.Dispose();
 		}
 	}
 }
