c4fcafdb67abf65f8bc52c3c4f8b055a0a732bd9
SirJosh3917 <joshuaneedham@hotmail.com>
2019-06-16 18:13:43 -0400

    read/write db tests

---
+++ tests/StringDB.Tests/DatabaseTests/ReadOnlyDatabaseTests.cs
@@ -0,0 +1,75 @@
+﻿using FluentAssertions;
+
+using Moq;
+
+using StringDB.Fluency;
+
+using System;
+using System.Collections.Generic;
+
+using Xunit;
+
+namespace StringDB.Tests.DatabaseTests
+{
+	public class ReadOnlyDatabaseTests
+	{
+		private readonly Mock<IDatabase<int, int>> _mock;
+		private readonly IDatabase<int, int> _rdb;
+		private readonly IDatabaseLayer<int, int> _dbLayer;
+
+		public ReadOnlyDatabaseTests()
+		{
+			_mock = new Mock<IDatabase<int, int>>();
+
+			_rdb = _mock.Object.AsReadOnly();
+			_dbLayer = (IDatabaseLayer<int, int>)_rdb;
+		}
+
+		[Fact]
+		public void InnerDatabase_IsDatabase()
+		{
+			_dbLayer.InnerDatabase.As<object>()
+				.Should().Be(_mock.Object);
+		}
+
+		[Fact]
+		public void Get_ShouldCall()
+		{
+			_rdb.Get(0);
+
+			_mock.Verify(x => x.Get(0));
+		}
+
+		[Fact]
+		public void TryGet_ShouldCall()
+		{
+			_rdb.TryGet(0, out _);
+
+			_mock.Verify(x => x.TryGet(0, out It.Ref<int>.IsAny));
+		}
+
+		[Fact]
+		public void GetAll_ShouldCall()
+		{
+			_rdb.GetAll(0);
+
+			_mock.Verify(x => x.GetAll(0));
+		}
+
+		[Fact]
+		public void Insert_ShouldThrow()
+		{
+			Action throws = () => _rdb.Insert(0, 0);
+
+			throws.Should().Throw<Exception>();
+		}
+
+		[Fact]
+		public void InsertRange_ShouldThrow()
+		{
+			Action throws = () => _rdb.InsertRange(new KeyValuePair<int, int>(0, 0));
+
+			throws.Should().Throw<Exception>();
+		}
+	}
+}
---
+++ tests/StringDB.Tests/DatabaseTests/WriteOnlyDatabaseTests.cs
@@ -0,0 +1,66 @@
+﻿using FluentAssertions;
+
+using Moq;
+
+using StringDB.Fluency;
+
+using System;
+using System.Collections.Generic;
+
+using Xunit;
+
+namespace StringDB.Tests.DatabaseTests
+{
+	public class WriteOnlyDatabaseTests
+	{
+		private readonly Mock<IDatabase<int, int>> _mock;
+		private readonly IDatabase<int, int> _wdb;
+		private readonly IDatabaseLayer<int, int> _dbLayer;
+
+		public WriteOnlyDatabaseTests()
+		{
+			_mock = new Mock<IDatabase<int, int>>();
+
+			_wdb = _mock.Object.AsWriteOnly();
+			_dbLayer = (IDatabaseLayer<int, int>)_wdb;
+		}
+
+		[Fact]
+		public void InnerDatabase_IsDatabase()
+		{
+			_dbLayer.InnerDatabase.As<object>()
+				.Should().Be(_mock.Object);
+		}
+
+		[Fact]
+		public void Get_ShouldThrow()
+			=> Throws(() => _wdb.Get(0));
+
+		[Fact]
+		public void TryGet_ShouldThrow()
+			=> Throws(() => _wdb.TryGet(0, out _));
+
+		[Fact]
+		public void GetAll_ShouldThrow()
+			=> Throws(() => _wdb.GetAll(0));
+
+		[Fact]
+		public void Insert_ShouldCall()
+		{
+			_wdb.Insert(0, 0);
+
+			_mock.Verify(x => x.Insert(0, 0));
+		}
+
+		[Fact]
+		public void InsertRange_ShouldCall()
+		{
+			_wdb.InsertRange(new KeyValuePair<int, int>(0, 0));
+
+			_mock.Verify(x => x.InsertRange(It.IsAny<KeyValuePair<int, int>[]>()));
+		}
+
+		private void Throws(Action action)
+			=> action.Should().Throw<Exception>();
+	}
+}
