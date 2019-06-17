354c06c488f327f5510497ba48dcdf870ec4867d
SirJosh3917 <joshuaneedham@hotmail.com>
2019-06-17 00:07:22 -0400

    database enumerable tests

--- tests/StringDB.Tests/QueryingTests/DatabaseEnumerableTests.cs
+++ tests/StringDB.Tests/QueryingTests/DatabaseEnumerableTests.cs
@@ -3,6 +3,7 @@ using StringDB.Querying;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 using Xunit;
 
 namespace StringDB.Tests.QueryingTests
@@ -28,5 +29,25 @@ namespace StringDB.Tests.QueryingTests
 				current++;
 			}
 		}
+
+		[Fact]
+		public void LockingEnumerable()
+		{
+			var integers = new int[] { 1, 2, 3 };
+
+			// i really don't know how to test that this works
+			// maybe if i were to mock the lock that'd probably work
+			var @lock = new SemaphoreSlim(1);
+
+			foreach(var i in integers.EnumerateWithLocking(@lock))
+			{
+				// the lock shouldn't be held at this point at least
+
+				@lock.Wait();
+				@lock.Release();
+			}
+		}
+
+		// the make train enumerable will be tested by the query manager part of the code
 	}
 }
