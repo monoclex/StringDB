1640ee9abd1290bc7652bf654abc92cc90f5dc59
SirJosh3917 <joshuaneedham@hotmail.com>
2019-09-15 21:30:15 -0400

    add base URL for compatability with github pages

--- _site/assets/js/just-the-docs.js
+++ _site/assets/js/just-the-docs.js
@@ -44,7 +44,7 @@ function initNav() {
 
 function initSearch() {
   var request = new XMLHttpRequest();
-  request.open('GET', 'http://localhost:4000/assets/js/search-data.json', true);
+  request.open('GET', '/assets/js/search-data.json', true);
 
   request.onload = function(){
     if (request.status >= 200 && request.status < 400) {
--- _site/build.bat
+++
@@ -1,5 +0,0 @@
-(
-	jekyll build
-	del /f /q ".\_site\build.bat"
-	echo all | xcopy /E ".\_site" ".\"
-)
